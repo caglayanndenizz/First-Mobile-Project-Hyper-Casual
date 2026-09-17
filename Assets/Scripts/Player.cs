3a6b737724c7876eb8466a3b0a80c40a6b2bcf86
Çağlayan Deniz Doğanay <caglayandenizd@gmail.com>
2026-09-17 17:10:34 +0300

    added
    grid system where player clones are created have been integrated. Working fine for now but in the future it could require some adjustment.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    //player clonelanmasi belki array izgarasi olusturularak cozulebilir.
-    //player in scale i yuzde 25 azaltilir , 10 adet nokta olusturulur belki de sirasiyla nerelerde olusturulacagini
-    // da ayarlayabiliriz. Bu sekilde playerin clone sayisi artar ve squadCount ile baglantili olur.
-    
-    
-    
+
+
     [Header("Player")]
     public Rigidbody2D playerRb;
     public int squadCount = 10;
+
+
+    [Header("Squad")]
+    public GameObject clonePrefab;
+
+    public int rows = 5;
+    public int columns = 5;
+    public float spacing = 2f;
+    public Vector2 startPosition = new Vector2(-1.5f, 1f);
+
+    Vector2[] positions;
+    GameObject[] clones;
 
     [Header("Bullet")]
     public float bulletSpawnInterval = 0.5f;
@@ -23,22 +31,26 @@ public class Player : MonoBehaviour
     public float baseBulletScale = 1f;
     private float bulletScale;
 
-    
+
 
     [Header("Swipe Movement")]
     public float swipeSpeed;
-    float target = -1.5f;
+    float target = 1.5f;
     Vector2 touchStartPos;
 
     [Header("UI")]
     public TextMeshPro squadCountText;
 
-    
+
     void Start()
     {
 
         playerRb = GetComponent<Rigidbody2D>();
+
+        CreateGrid();
+        CreateClones();
         UpdateBulletScale();
+        UpdateClones();
         StartCoroutine(ShootInterval());
         UpdateSquadCountText();
     }
@@ -85,11 +97,61 @@ public class Player : MonoBehaviour
         //belirledigimiz hizda playerin x pozisyonunu targete dogru gercek zamanli hareket ettir.
     }
 
+    void CreateGrid() //sadece yumurta kolisi gorevinde.
+    //yumurtalar farkli metodda yerlestirilcek.
+    {
+        positions = new Vector2[rows * columns];
+
+        int index = 0; // index in isi o an kacinci gozde oldugunu bilmek.
+
+        //dongunun disinda oldugu zaman 0 olarak belirlenir ve surekli artar.
+        //disinda olmazsa her turda tekrar 0 olur.
+
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < columns; col++)
+            {
+                float x = startPosition.x + col * spacing;
+                float y = startPosition.y - row * spacing;
+                //koordinat sistemine gore asagi inilmek istediginde - olacak cunku sutunlari saga dogru
+                //yaratiyoruz satirlari ise asagiya dogru.
+
+                positions[index] = new Vector3(x, y, 0);
+                index++;
+            }
+        }
+    }
+
+    void CreateClones()
+    {
+        clones = new GameObject[positions.Length];
+        //olusturulan nokta sayisi kadar clone yaratilir.
+
+        for (int i = 0; i < positions.Length; i++)
+        {
+            GameObject clone = Instantiate(clonePrefab, transform); //transform = lider player
+            clone.transform.localPosition = positions[i]; //yaratilan clone un pozisyonunu griddeki siradaki yere yerlestir.
+            clone.transform.localScale = Vector2.one; //yaratilan clone un scale i liderle ayni olsun.
+            clone.SetActive(false); //simdilik aktif olmasin.
+            clones[i] = clone; //yaratilan clone u grid e yerlestir.
+        }
+
+    }
     
+    void UpdateClones()
+    {
+        for (int i = 0; i < clones.Length; i++)
+        {
+            clones[i].SetActive(i < squadCount - 1);
+        }
+    }
+
     public void SetSquad(int newCount)
     {
         squadCount = Mathf.Max(newCount, 0);
         UpdateBulletScale(); //squadcount a bakarak bulletscale kontrol ediliyor.
+        UpdateClones();
         UpdateSquadCountText();
 
         if (squadCount < 1) GameOver();
@@ -103,12 +165,23 @@ public class Player : MonoBehaviour
         }
     }
 
-    void BulletSpawn()
+    void Shoot(Vector3 position)
     {
-        GameObject b = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+        GameObject b = Instantiate(bulletPrefab, position, Quaternion.identity);
         b.transform.localScale = bulletPrefab.transform.localScale * bulletScale;
         b.GetComponent<Bullet>().bulletDamage = Mathf.RoundToInt(baseBulletDamage * bulletScale);
-        
+    }
+
+    void BulletSpawn()
+    {
+        Shoot(bulletSpawnPoint.position);
+        for (int i = 0; i < clones.Length; i++)
+        {
+            if (clones[i].activeSelf)
+            {
+                Shoot(clones[i].transform.position);
+            }
+        }
     }
 
     IEnumerator ShootInterval()
@@ -131,13 +204,16 @@ public class Player : MonoBehaviour
     public void UpdateBulletScale()
     {
         float multiplier;
-        if(squadCount >= 50) multiplier = 2.0f;
-        else if(squadCount >= 25) multiplier = 1.5f;
-        else if(squadCount >= 10) multiplier = 1.3f;
+        if (squadCount >= 50) multiplier = 2.0f;
+        else if (squadCount >= 25) multiplier = 1.5f;
+        else if (squadCount >= 10) multiplier = 1.3f;
         else multiplier = 1.0f;
 
         bulletScale = baseBulletScale * multiplier;
     }
+
+    
+    
 
 
 
